bd474de82c91fc905817ba50b26b20169d463a3b
Murad Nurmukhamedov <mns130691@gmail.com>
2020-08-27 15:03:37 +0300

    create AdminUserController

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::resource('admin/users', "AdminUsersController");
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
